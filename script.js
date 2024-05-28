dc213c16f210eab4fc58e650ae2b1dc719ffc45c
ederdsc <edersantoscamargo@gmail.com>
2024-05-28 20:46:04 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function togglemode() {
+  const html = document.documentElement
+
+  // if(html.classList.contains("light")){
+  //  html.classList.remove("light")
+  // } else {
+  //  html.classList.add("light")
+  // }
+  // COMANDO ABAIXO REALIZA TODAS FUNÇÕES DO CÓDIGO ACIMA!!!! //
+
+  html.classList.toggle("light")
+  
+//pegar a tag img
+  const img = document.querySelector("#profile img")
+  //substituir a imagem
+  if(html.classList.contains("light")){
+    //se tiveer light mode, adicionar a imagem light
+    img.setAttribute("src", "/assets/avatar-lighttt.png")
+  
+  } else{
+    //se tiver sem light mode, manter imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
+
