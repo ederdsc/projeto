1d3374539ed5fe553a189465a5966a55c4c58414
ederdsc <edersantoscamargo@gmail.com>
2024-06-04 03:45:16 -0300

    adjusting avatar in light mode

--- script.js
+++ script.js
@@ -9,17 +9,16 @@ function togglemode() {
   // COMANDO ABAIXO REALIZA TODAS FUNÇÕES DO CÓDIGO ACIMA!!!! //
 
   html.classList.toggle("light")
-  
-//pegar a tag img
+
+  //pegar a tag img
   const img = document.querySelector("#profile img")
   //substituir a imagem
-  if(html.classList.contains("light")){
+  if (html.classList.contains("light")) {
     //se tiveer light mode, adicionar a imagem light
-    img.setAttribute("src", "/assets/avatar-lighttt.png")
-  
-  } else{
+    img.setAttribute("src", "/assets/avatar-light.png")
+  } 
+  else {
     //se tiver sem light mode, manter imagem normal
     img.setAttribute("src", "./assets/avatar.png")
   }
 }
-
